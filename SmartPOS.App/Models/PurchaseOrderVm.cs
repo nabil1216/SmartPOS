c26b40da2468c24265d67d673ad9ded349e1f250
nabil1216 <nabil1216@gmail.com>
2018-03-14 13:56:35 +0600

    Purchase Oder Modification
    Purchase Oder Modification

--- SmartPOS.App/Models/PurchaseOrderVm.cs
+++ SmartPOS.App/Models/PurchaseOrderVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace SmartPOS.App.Models
     public class PurchaseOrderVm
     {
         public int Id { get; set; }
+        [DisplayName("Model No")]
         public string ModelNo { get; set; }
+        [DisplayName("Product Name")]
         public string ProductName { get; set; }
     }
 }
